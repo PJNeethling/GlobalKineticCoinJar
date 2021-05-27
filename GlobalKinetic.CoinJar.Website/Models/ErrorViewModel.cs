2ac79275ccdc900c83ffaa6a182e32efe91d506f
PJNeethling <neethlingpeterjohn@gmail.com>
2021-05-27 16:48:47 +0200

    Initial Website Deployment
    Initial Website Deployment
    Blank Site

---
+++ GlobalKinetic.CoinJar.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GlobalKinetic.CoinJar.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
